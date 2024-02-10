5223a548d6b8716566f17ca493262c95318cdaa4
Javier Ruedas <jruedas@twc.com>
2024-02-10 08:47:49 -0600

    complete reverse string

--- exercises/reversestring/index.js
+++ exercises/reversestring/index.js
@@ -6,6 +6,31 @@
 //   reverse('hello') === 'olleh'
 //   reverse('Greetings!') === '!sgniteerG'
 
-function reverse(str) {}
+// solution 1: reverse() 
+// function reverse(str) {
+//     return str.split('').reverse().join('');
+// }
+
+// solution 2: for loop
+// function reverse(str) {
+//     let newStr = '';
+//     for (let i = str.length-1; i >= 0; i--){
+//         newStr += str[i];
+//     }
+//     return newStr;
+// }
+
+// solution 3: for of loop
+// function reverse(str){
+//     let newStr = '';
+//     for (let char of str){
+//        newStr =  char + newStr;
+//     }
+//     return newStr;
+// }
+
+function reverse(str){
+  str.split('').reduce((newStr, char)=> char + newStr, '');
+}
 
 module.exports = reverse;
